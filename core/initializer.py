007c954d6d623387565e062d87274f4a0435dcb2
niejianfei <11814143@qq.com>
2023-03-29 21:24:27 +0800

    commit

---
+++ core/initializer.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def normal_init(shape):
+    """normal init"""
+    return np.random.normal(loc=0, scale=1, size=shape)
+
+
+def zeros_init(shape):
+    """zero init"""
+    return np.zeros(shape)
+
+
+if __name__ == '__main__':
+    shape1 = (12, 784)
+    init = zeros_init
+    print(init)
+    print(type(init))
--- core/layer.py
+++ core/layer.py
@@ -1,17 +1,97 @@
 import numpy as np
+from initializer import zeros_init
+from initializer import normal_init
 
-'''model进行forward和backward，其实底层都是网络层在进行实际运算，因此网络层需要有提供forward和backward接口进行对应的运算。同时还应该将该层的参数和梯度记录下来。'''
 
-
+# model进行forward和backward，其实底层都是网络层在进行实际运算，因此网络层需要有提供forward和backward接口进行对应的运算。
+# 同时还应该将该层的参数和梯度记录下来
 class Layer(object):
-    def __int__(self, name):
+    def __init__(self, name):
         self.name = name
-        self.params, self.grad = None, None
+        self.params, self.grads = None, None
 
     def forward(self, inputs):
         raise NotImplementedError
 
-    def backward(self, outputs):
+    def backward(self, grad):
         raise NotImplementedError
 
 
+'''
+全连接层：
+forward
+k -> k+1层
+activation(X(k) * W(K+1) + b(k+1)) = X(k+1)
+batch_size * num_inputs => num_inputs * num_outputs + 1 * num_outputs => activation => batch_size * num_outputs
+
+backward
+k+1 -> k层:
+对W导数：X(k).T * grad(X(k+1)), (num_inputs * batch_size) * (batch_size * num_outputs)
+对b导数: grad(X(k+1)), 1 * num_outputs
+'''
+
+
+class Dense(Layer):
+    def __init__(self, num_in, num_out,
+                 w_init=normal_init,
+                 b_init=zeros_init):
+        super().__init__("Linear")
+
+        self.params = {
+            "w": w_init((num_in, num_out)),
+            "b": b_init((1, num_out))
+        }
+
+        self.inputs = None
+
+    def forward(self, inputs):
+        self.inputs = inputs
+        return inputs @ self.params["w"] + self.params["b"]
+
+    def backward(self, grad):
+        self.grads["w"] = self.inputs.T @ grad
+        self.grads["b"] = np.sum(grad, axis=0)
+        return grad @ self.params["w"].T
+
+
+# 激活函数可以看做是一种网络层，同样需要实现forward和backward方法。
+class Activation(Layer):
+    """Base activation layer"""
+
+    def __init__(self, name):
+        # super()函数是用来调用父类的方法的,它可以让子类调用父类的方法,从而实现代码的复用
+        # 在调用super()函数时，若传递self和name作为参数。由于super()函数中的第一个参数是类，而不是实例，因此会引发Unexpected argument错误。
+        super().__init__(name)
+        self.inputs = None
+
+    def forward(self, inputs):
+        self.inputs = inputs
+        return self.func(inputs)
+
+    def backward(self, grad):
+        return self.derivative_func(self.inputs) * grad
+
+    def func(self, x):
+        raise NotImplementedError
+
+    def derivative_func(self, x):
+        raise NotImplementedError
+
+
+class ReLU(Activation):
+    """ReLU activation function"""
+
+    def __init__(self):
+        super().__init__("ReLU")
+
+    def func(self, x):
+        return np.maximum(x, 0.0)
+
+    def derivative_func(self, x):
+        # true:1, false:0
+        return x > 0
+
+
+if __name__ == '__main__':
+    a = Activation("ll").name
+    print(a)
